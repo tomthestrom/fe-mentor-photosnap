065f541cec9361508e0a7cfa5f35b014a84a0469
tomthestrom <thruska29@gmail.com>
2021-05-19 13:52:47 +0200

    pricing features done

--- config/pricing-mock.cfg.js
+++ config/pricing-mock.cfg.js
@@ -28,7 +28,54 @@ var pricingDB = {
     },
   ],
   features: [
-
+    {
+      name: "Unlimited Story Posting",
+      plan_0: true,
+      plan_1: true,
+      plan_2: true
+    },
+    {
+      name: "Unlimited Photo Upload",
+      plan_0: true,
+      plan_1: true,
+      plan_2: true
+    },
+    {
+      name: "Embedding Custom Content",
+      plan_0: false,
+      plan_1: true,
+      plan_2: true
+    },
+    {
+      name: "Customize Metadata",
+      plan_0: false,
+      plan_1: true,
+      plan_2: true
+    },
+    {
+      name: "Advanced Metrics",
+      plan_0: false,
+      plan_1: false,
+      plan_2: true
+    },
+    {
+      name: "Photo Downloads",
+      plan_0: false,
+      plan_1: false,
+      plan_2: true
+    },
+    {
+      name: "Search Engine Indexing",
+      plan_0: false,
+      plan_1: false,
+      plan_2: true
+    },
+    {
+      name: "Custom Analytics",
+      plan_0: false,
+      plan_1: false,
+      plan_2: true
+    },
   ]
 };
 
